80bdbc388830acb6319d4894248d9a5475533bbf
MacBookPro <t-saito@MacBookPronoMacBook-Pro.local>
2020-06-20 13:16:21 +0900

    要修正

--- formdemo/urls.py
+++ formdemo/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'formdemo'
 
 urlpatterns = [
-    path('', views.formdemo_list, name='formdemo_list'),
+    path('', views.formdemo_top, name ='formdemo_top'),
+    path('create/', views.formdemo_create, name='formdemo_create'),
+    path('detail/<int:pk>/', views.formdemo_detail, name='formdemo_detail'),
+#    path('update/<int:pk>/', views.formdemo_update, name='formdemo_update'),
+    path('update/', views.formdemo_update, name='formdemo_update'),
+    path('delete/', views.formdemo_delete, name='formdemo_delete'),
 ]
--- formdemo/views.py
+++ formdemo/views.py
@@ -1,10 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Formdemo
 from .forms import FormdemoCreateForm
 
 # Create your views here.
-def formdemo_list(request):
+def formdemo_create(request):
+
+    form = FormdemoCreateForm(request.POST or None)
+    if request.method == 'GET':
+
+        context = {
+            'formdemo_create' : FormdemoCreateForm()
+        }
+        return render(request, 'formdemo/formdemo_create.html', context)
+
+    else:
+
+        form.save()
+        return redirect('/formdemo')
+
+def formdemo_top(request):
+    return render(request, 'formdemo/formdemo_top.html')
+
+def formdemo_detail(request, pk):
+
+    details = Formdemo.objects.get(pk=pk)
     context = {
-        'formdemo_list' : FormdemoCreateForm()
+        'details' : details,
     }
-    return render(request, 'formdemo/formdemo_list.html', context)
+    return render(request, 'formdemo/formdemo_detail.html', context)
+
+def formdemo_update(request):
+    return render(request, 'formdemo/formdemo_update.html')
+    
+"""
+def formdemo_update(request, pk):
+    messages = get_object_or_404(Formdemo, pk=pk)
+    form = FormdemoCreateForm(request.POST or None, instance=messages)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('/formdemo')
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'formdemo/formdemo_update.html', context)
+"""
+def formdemo_delete(request):
+    return render(request, 'formdemo/formdemo_delete.html')
